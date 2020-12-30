f2224137965e59201cb5d99571803eb804926f29
AkshayaPk <ashpeekay23@gmail.com>
2020-12-30 16:02:32 +0530

    Adding security group rules

--- test/fixtures/kitchen.tf
+++ test/fixtures/kitchen.tf
@@ -15,6 +15,15 @@ module "eks-test" {
   }
   vpc_id     = "vpc-368a7b4b"
   subnet_ids = ["subnet-83a90ce5", "subnet-7335932c", "subnet-298cf964", "subnet-f349e9d2", "subnet-5d62fd53"]
+  custom_rules = {
+    nodeportapp = {
+      type = "ingress"
+      from_port = 31000
+      to_port = 33000
+      protocol = "tcp"
+      cidr_blocks = ["27.5.177.248/32"]
+    }
+  }
   tags = {
     "Environment" = "Backbase-dev"
   }
