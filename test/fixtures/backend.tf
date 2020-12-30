d81a2b9ab3396bc50b48c41b8ec461c6ead08a39
AkshayaPk <ashpeekay23@gmail.com>
2020-12-31 00:25:41 +0530

    Added encrypt option

--- test/fixtures/backend.tf
+++ test/fixtures/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "backbase-terraform-backend"
+    encrypt        = true
     dynamodb_table = "backbase-tf-lock"
     region         = "us-east-1"
     key            = "tfstate/terraform.tfstate"
